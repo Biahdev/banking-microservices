ef3e93c66530f026531554f3de15b7fbb714458b
Ana Beatriz de Souza <abeatriz.dev@gmail.com>
2025-06-06 21:53:05 -0300

    feat: criando a entidade Account

---
+++ account-service/src/main/java/br/com/abeatrizdev/account_service/entity/Account.java
@@ -0,0 +1,44 @@
+package br.com.abeatrizdev.account_service.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private UUID publicId;
+
+    String name;
+
+    String document;
+
+    BigDecimal balance;
+
+    AccountStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void generatePublicId() {
+        if (publicId == null) {
+            publicId = UUID.randomUUID();
+        }
+    }
+
+
+}
---
+++ account-service/src/main/java/br/com/abeatrizdev/account_service/entity/AccountStatus.java
@@ -0,0 +1,6 @@
+package br.com.abeatrizdev.account_service.entity;
+
+public enum AccountStatus {
+    ACTIVE,
+    INACTIVE
+}
